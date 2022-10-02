53d09ca6d96a5d6ff848bf078dede663ceaa2c0c
Nate <kings_el@yahoo.com>
2022-10-02 13:59:50 -0600

    removed unnecessary divs and vairables, handling image files, fixed background image not showing in index

--- next.config.js
+++ next.config.js
@@ -2,8 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['ipfs.io', 'www.macmillandictionary.com', 'QmSLxBtAQo5a443mduMsCTYpaxPNL27uskKvCk9NVnoWPU', ''],
+    domains: [
+      "ipfs.io",
+      "www.macmillandictionary.com",
+      "QmSLxBtAQo5a443mduMsCTYpaxPNL27uskKvCk9NVnoWPU",
+      "gateway.ipfscdn.io",
+    ],
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,100 +1,86 @@
 import type { GetServerSideProps, NextPage } from "next";
-import { getTokenMetadata } from "../scripts/Metadata.js";
-import styles from "../styles/Home.module.css";
 import Link from "next/link";
 import {
   MediaRenderer,
   useActiveListings,
   useMarketplace,
 } from "@thirdweb-dev/react";
-import { useRouter } from "next/router";
 import ImageBox from "../components/ImageBox";
-import Button from "../components/Button";
-import { handleGetNftData } from "../data/handleGetNftData";
 import FeaturedNftsGrid from "../components/FeaturedNftsGrid";
-import fmLogo from "../public/images/fm_logo.png";
-import { Nft } from "@alch/alchemy-sdk";
-import afututuremodern from "../data/LaunchArtists/afuturemodern.json";
+import bg from "../public/images/shapes_bg.png";
+import { AuctionListing, DirectListing } from "@thirdweb-dev/sdk";
 
 const enum ImageDimensions {
   width = 368,
   height = 368,
 }
 
-interface NewHomeProps {
-  nftData: Nft;
-  imageGalleryData: Nft[];
-}
-
-export const getStaticProps: GetServerSideProps<NewHomeProps> = async (
-  context
-) => {
-  // Call an external API endpoint to get posts.
-  const nftData =
-    (await getTokenMetadata(
-      afututuremodern.afuturemodern.artworks[0].token_address,
-      afututuremodern.afuturemodern.artworks[0].token_id
-    )) || null;
-
-  const imageGalleryData = await handleGetNftData();
-
-  return {
-    props: {
-      nftData: JSON.parse(JSON.stringify(nftData)),
-      imageGalleryData: JSON.parse(JSON.stringify(imageGalleryData)),
-    },
-  };
-};
-
-const Home: NextPage<NewHomeProps> = ({ imageGalleryData, nftData }) => {
-  const router = useRouter();
-
+const Home: NextPage = () => {
   // Connect your marketplace smart contract here (replace this address)
   const marketplace = useMarketplace(
     process.env.NEXT_PUBLIC_MARKETPLACE_CONTRACT_ADDRESS // Your marketplace contract address here
   );
 
-  const { data: listings, isLoading: loadingListings } =
-    useActiveListings(marketplace);
+  const { data: listings, isLoading: loadingListings } = useActiveListings(
+    marketplace,
+    {
+      // limits amount of nfts to 9 in the featured artists grid
+      count: 9,
+    }
+  );
 
-  const imageGridItems = listings?.map((listing) => (
-    <div
-      key={listing.id}
-      className={"fmbs-gallery-grid-item"}
-      // onClick={() => router.push(`/listing/${listing.id}`)}
-    >
-      <>
-        {/* <h2>{nftData?.rawMetadata?.name}</h2> */}
-        {/* <h2>{listing?.asset?.name}</h2> */}
+  const handleGetListingMediaType = (
+    listing: AuctionListing | DirectListing
+  ): JSX.Element => {
+    if (listing.asset.animation_url?.endsWith(".mp4")) {
+      return (
+        <video
+          poster={listing?.asset?.image as string}
+          width={ImageDimensions.width.toString() + "px"}
+          height={ImageDimensions.height.toString() + "px"}
+          src={listing?.asset?.animation_url as string}
+          controls={true}
+        />
+      );
+    } else if (listing.asset.image?.endsWith(".png")) {
+      return (
+        <ImageBox
+          width={ImageDimensions.width.toString() + "px"}
+          height={ImageDimensions.height.toString() + "px"}
+          src={listing?.asset?.image as string}
+          id={listing.id}
+          backgroundColor={"#111111FF"}
+          alt={listing.asset.name || "Art"}
+        />
+      );
+    } else return <span>media not supported</span>;
+  };
 
-        <h2>
-          <Link href={`/listing/${listing.id}`}>
-            <a>{listing.asset.name}</a>
-          </Link>
-        </h2>
+  const imageGridItems = listings?.map((listing) => {
+    return (
+      <div key={listing.id} className={"fmbs-gallery-grid-item"}>
+        <>
+          {handleGetListingMediaType(listing)}
 
-        <p>{listing?.asset?.description}</p>
-        {
-          <video
-            poster={listing?.asset?.image as string}
-            width={ImageDimensions.width.toString() + "px"}
-            height={ImageDimensions.height.toString() + "px"}
-            src={listing?.asset?.animation_url as string}
-            controls={true}
-          />
-        }
-      </>
-
-      <p>
-        <b>{listing.buyoutCurrencyValuePerToken.displayValue}</b>{" "}
-        {listing.buyoutCurrencyValuePerToken.symbol}
-      </p>
-    </div>
-  ));
+          <h2>
+            <Link href={`/listing/${listing.id}`}>
+              <a>{listing.asset.name}</a>
+            </Link>
+          </h2>
+        </>
+      </div>
+    );
+  });
 
   return (
     <div className="fmbs-bg-wrapper">
-      <div className="fmbs-bg fmbs-bg--shapes"></div>
+      <div
+        className="fmbs-bg"
+        style={{
+          backgroundImage: `url(${bg.src})`,
+          backgroundSize: `100% auto`,
+        }}
+      ></div>
       <div className="fmbs-gallery fmbs-page-content">
         <>
           {loadingListings ? (
@@ -102,9 +88,7 @@ const Home: NextPage<NewHomeProps> = ({ imageGalleryData, nftData }) => {
           ) : (
             <FeaturedNftsGrid>{imageGridItems}</FeaturedNftsGrid>
           )}
-          <Button href="javascript://" value="Browse" />
         </>
-        {/* <FeaturedNftsGrid>{imageGridItems}</FeaturedNftsGrid> */}
       </div>
     </div>
   );
